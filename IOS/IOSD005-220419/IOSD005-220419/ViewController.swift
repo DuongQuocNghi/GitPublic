74c9b7f3f79b1446720bdaceedc78fa4ba6f33dd
Nghi Mac <nghi.duong@bizcellsoft.com>
2019-04-22 21:18:24 +0700

    bh 220419

---
+++ IOS/IOSD005-220419/IOSD005-220419/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSD005-220419
+//
+//  Created by ADMIN on 4/22/19.
+//  Copyright © 2019 ADMIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
