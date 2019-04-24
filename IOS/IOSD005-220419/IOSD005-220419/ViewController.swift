6bca5a86b2d05abd80db3718fb5256118be530e6
Nghi Mac <nghi.duong@bizcellsoft.com>
2019-04-24 21:13:22 +0700

    bh 240419

--- IOS/IOSD005-220419/IOSD005-220419/ViewController.swift
+++ IOS/IOSD005-220419/IOSD005-220419/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var userID: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var createBtn: UIButton!
+    @IBOutlet weak var borderInput: UIView!
+    
+    override func viewDidLayoutSubviews() {
+        setUpView()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func setUpView() {
+        logo.layer.cornerRadius = 5
+        loginBtn.layer.cornerRadius = 5
+        
+        createBtn.layer.cornerRadius = 5
+        createBtn.layer.borderWidth = 1
+        createBtn.layer.borderColor = UIColor.init(red: 102/255, green: 140/255, blue: 190/255, alpha: 1).cgColor
+        
+        borderInput.layer.cornerRadius = 5
+        borderInput.layer.borderWidth = 1
+        borderInput.layer.borderColor = UIColor.init(red: 218/255, green: 219/255, blue: 220/255, alpha: 1).cgColor
+        
+    }
 
 }
 
