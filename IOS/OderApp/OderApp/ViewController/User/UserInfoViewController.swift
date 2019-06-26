15cab5d6fec8ee1f6bdaa931bd43dcc36a59f6ee
Nghi Mac <nghi.duong@bizcellsoft.com>
2019-06-26 21:27:59 +0700

    bh 26 06

---
+++ IOS/OderApp/OderApp/ViewController/User/UserInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserInfoViewController.swift
+//  OderApp
+//
+//  Created by ADMIN on 6/26/19.
+//  Copyright © 2019 ADMIN. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
